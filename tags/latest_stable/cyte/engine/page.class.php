1be3e9a124b8e645a7f1ad445d207a651e057ad9
thomas.welfley <thomas.welfley@f2977805-363b-0410-8f67-fbf977c41358>
2008-12-16 19:30:29 +0000

    Latest stable is dumb.

--- tags/latest_stable/cyte/engine/page.class.php
+++ tags/latest_stable/cyte/engine/page.class.php
@@ -91,7 +91,7 @@ class page {
 		
 		# Get default values
 		$template_file              = $template_conf['def_template'];
-		$title                 	 	= new title();
+		$title                 	 	= $site_conf['title'];
 		$auth_requirement           = $template_conf['def_auth_req'];
 		$auth_form                  = $template_conf['def_auth_form'];
 		$auth_routine               = $template_conf['def_auth_routine'];
@@ -102,7 +102,7 @@ class page {
 		   $template_file = $args['template'];
 		}
 		if(isset($args['title'])) {
-		   $title->page_title = $args['title'];
+		   $title->page_title = $title.': '.$args['title'];
 		}
 		if(isset($args['auth_requirement'])) {
 		   $auth_requirement = $args['auth_requirement'];
